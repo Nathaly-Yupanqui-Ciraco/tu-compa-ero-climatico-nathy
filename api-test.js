6a9235b8ea82240886efd8593f4fd5f47df0ca42
Pavel <1539921@senati.pe>
2026-04-07 13:38:27 -0500

    Resolviendo ultimos cambios para el funcionamiento

--- api-test.js
+++ api-test.js
@@ -1,12 +1,11 @@
-const claveApi = "358bb33e61c64b499b352348260704";
+const claveApi = "01195b7eabea47e1a12202036250403";
 
 const idioma = "es";
 const ciudad = "Huancayo";
 
 const apiClimaActual = `https://api.weatherapi.com/v1/current.json?q=${ciudad}&lang=${idioma}&key=${claveApi}`;
 
-fetch(apiClimaActual)
-    .then(response => response.json())
-    .then(data => {
-        console.log(data);
-    });
+const response = await fetch(apiClimaActual);
+let data = await response.json();
+
+console.log(data.current.condition);
---
+++ codigo.js
@@ -0,0 +1,31 @@
+const claveApi  = '01195b7eabea47e1a12202036250403';
+const idioma    = 'es';
+const inpCiudad    = document.getElementById('input-ciudad');
+
+async function obtenerClima() {
+
+    const ciudad = inpCiudad.value;
+
+    if(!ciudad) {
+        alert('Por favor, ingresa el nombre de una ciudad');
+        return;
+    }
+
+    const apiClimaActual = `https://api.weatherapi.com/v1/current.json?q=${ciudad}&lang=${idioma}&key=${claveApi}`;
+
+    const response = await fetch(apiClimaActual);
+    const data = await response.json();
+    
+    mostrarClima(data);
+}
+
+function mostrarClima(data) {
+    document.querySelector('.clima-icono').src = data.current.condition.icon;
+    document.querySelector('.clima-texto').innerHTML = data.current.condition.text;
+    document.querySelector('.temp').innerHTML = data.current.temp_c + '°C';
+    document.querySelector('.ciudad').innerHTML = data.location.name;
+    document.querySelector('.humedad').innerHTML = data.current.humidity + '%';
+    document.querySelector('.viento').innerHTML = data.current.wind_kph + ' km/h';
+
+    document.querySelector('clima-contenedor').style.display = 'block';  
+}
